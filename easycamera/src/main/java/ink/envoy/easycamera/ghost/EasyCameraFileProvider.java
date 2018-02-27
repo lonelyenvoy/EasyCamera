58009f66b55696ec02c600e337b92ec573283fcf
lonelyenvoy <lonelyenvoy@gmail.com>
2018-02-27 15:34:57 +0800

    upgrade to v0.0.3: fix issue #1

---
+++ easycamera/src/main/java/ink/envoy/easycamera/ghost/EasyCameraFileProvider.java
@@ -0,0 +1,15 @@
+package ink.envoy.easycamera.ghost;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by LonelyEnvoy on 2018/2/27.
+ */
+
+/**
+ * An empty wrapper of
+ * @see android.support.v4.content.FileProvider
+ * solving issue #1
+ */
+public class EasyCameraFileProvider extends FileProvider {
+}
